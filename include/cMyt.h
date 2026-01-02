bd8b4a372aae96d9c4b37e0d08f9060d50237bf9
CmPi <cmpi@webe.fr>
2026-01-02 14:26:09 +0100

    v 0.0.0.1

---
+++ include/cMyt.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class cMyt {
+public:
+    void begin();
+    int compute(int x);
+};
---
+++ src/cMyt.cpp
@@ -0,0 +1,9 @@
+#include "cMyt.h"
+
+void cMyt::begin() {
+    // init
+}
+
+int cMyt::compute(int x) {
+    return x * 2;
+}
